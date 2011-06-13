cda23f0a17b54cf37f716c6ba2e84836a9891c86
Stuart Wu <stuart@thepolydice.com>
2011-06-13 15:05:33 +0800

    Modified to show image and stop font size adjustment.

---
+++ MyRssReader/MyTableSubtitleItemCell.h
@@ -0,0 +1,16 @@
+//
+//  MyTableSubtitleItemCell.h
+//  MyRssReader
+//
+//  Created by Ching on 2011/6/10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyTableSubtitleItemCell : TTTableSubtitleItemCell {
+    
+}
+
+@end
---
+++ MyRssReader/MyTableSubtitleItemCell.m
@@ -0,0 +1,14 @@
+//
+//  MyTableSubtitleItemCell.m
+//  MyRssReader
+//
+//  Created by Ching on 2011/6/10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyTableSubtitleItemCell.h"
+
+
+@implementation MyTableSubtitleItemCell
+
+@end
